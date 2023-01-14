066954051d56c9629bb8a6238474289e76b228ac
nicolaspborges96 <nicolaspborges96@gmail.com>
2023-01-14 10:41:00 -0300

    1 commit

---
+++ src/components/menu/menu.component.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+const Menu = () => {
+    return (
+        <div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    )
+
+}
+
+export default Menu;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,11 @@
+import Menu from "../../components/menu/menu.component";
+
+
+const Home = () => {
+
+    return (
+        <Menu />
+    )
+}
+
+export default Home;
